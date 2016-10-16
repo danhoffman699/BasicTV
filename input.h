406a8451d3dfdc44b773dd58cef753ccc12d1898
Daniel Hoffman <danielhoffman699@gmail.com>
2016-10-16 11:23:46 -0500

    docs for input and IR, shell for input code

--- input.h
+++ input.h
@@ -1,6 +1,48 @@
 #ifndef INPUT_H
 #define INPUT_H
+
 /*
-  input.h: reads inputs from respective APIs and creates
+  INPUT_KEY_* is reserved from 32 to 127 for ASCII (extended
+  ASCII isn't used). Anything above 127 is free game, so space
+  isn't an issue.
  */
+
+#define INPUT_KEY_POWER 1
+#define INPUT_KEY_MENU 2
+#define INPUT_KEY_UP 3
+#define INPUT_KEY_DOWN 4
+#define INPUT_KEY_LEFT 5
+#define INPUT_KEY_RIGHT 6
+#define INPUT_KEY_ENTER 7
+#define INPUT_KEY_CH_UP 8
+#define INPUT_KEY_CH_DOWN 9
+#define INPUT_KEY_VOL_UP 10
+#define INPUT_KEY_VOL_DOWN 11
+#define INPUT_KEY_STAR 12
+#define INPUT_KEY_POUND 13
+#define INPUT_KEY_MODIFIER 14
+#define INPUT_KEY_MODIFIER_CANCEL 15
+
+/*
+  ASCII is allowed as input, which is pretty dope. However,
+  it doesn't natively work with IR. Create a modifier key that
+  maps the numerical input to a number pad keyboard and press
+  whatever the modifier cancel button is.
+ */
+
+#define INPUT_IS_ASCII(x) (x >= 32 && x <= 126)
+#define INPUT_IS_LETTER(x) (x >= 'A' && x <= 'z')
+#define INPUT_IS_NUMBER(x) (x >= '0' && x <= '9')
+
+/*
+  input.h: reads inputs from respective APIs and adds to a universal
+  buffer. Data can not be read out of order.
+
+  TODO: Do more serious work with this input API. I'm not too concerned
+  now because the internal networking is more important, but I did what
+  I did as a proof of concept and to lay down some docs for the standard
+ */
+
+void input_init();
+
 #endif
--- ir.h
+++ ir.h
@@ -5,29 +5,60 @@
   ir.h: basic input handling for IR sensor. The part is the TSOP38238
   for the Raspberry Pi (but via GPIO, so anything should work). 
 
-  Modifier key combinations are used extensively for input. The only
-  purpose of this file is to interface with LIRC to extract keycodes. 
-  This file is referenced by input.cpp & input.h (not created yet) and
-  will extract the key combinations from there.
-
-  This implements the NEC protocol (8-bit command length) and does basic
-  error checking, correcting, and repeat commands
-
-  This is the point where I gave up on Windows support, because LIRC seems
-  like the best way forward for this.
+  This implements the NEC protocol and does basic error checking, correcting, 	
+  and repeat commands. Pretty much everything it should do
+  
+  I don't think LIRC is the way to go, especially with the programmable 
+  aspect of it. Linux drivers exist that can read GPIO pins at 38kHz, so
+  I am planning on using that in conjunction with this.
  */
 
-#define IR_COMMAND_BUFFER_SIZE 32
-
 #include "array"
+#include "tuple"
+#include "input.h"
+
+/*
+  I wish I had a remote with 64K keys...
+ */
+
+#define IR_LOOKUP_SIZE 65536
+#define IR_BACKLOG_SIZE 65536
 
 struct ir_remote_t{
 private:
+	/*
+	  This contains:
+	  The address (16-bit) inside of the NEC standard (8-bit was used with
+	  error detection, but that was removed in favor of more space)
+	  The command sent over. This varies heavily on the remote used, and is
+	  not used directly anywhere else in the program
+	  The key as the software sees it. This contains the keys on the remote
+	  in a (fairly) easy to read macro setup.
+	 */
+	std::array<std::tuple<uint16_t, uint8_t, uint64_t>, IR_LOOKUP_SIZE> lookup;
+	/*
+	  The backlog contains all of the general input API  codes in the order 
+	  they were sent (new ones are at the end, so old to new). The only time
+	  that there is an interdependency is when the modifier keys is pressed,
+	  and then it looks forward one by one until a valid string is typed. 
+	  Forward this information into the general input API and have that deal
+	  with the actual code being ran.
+
+	  TODO: implement a cancel button for the modifier. I'm having trouble
+	  thinking of what that key should be...
+	 */
+	std::array<uint64_t, IR_BACKLOG_SIZE> backlog;
 public:
 	data_id_t id;
+	/*
+	  I can't think of a good reason to get keys out of order
+	  Returns the input key
+	 */
+	uint64_t get_key();
 	ir_remote_t();
 	~ir_remote_t();
-	
 };
+
+void ir_init(); // ir_loop is statically declared
 
 #endif
